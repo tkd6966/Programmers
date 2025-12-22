2be855646bdc6f5f35b9890f8ed7ef63211ad464
tkd6966 <tkd6966@gmail.com>
2025-12-22 09:41:09 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+    if(num1 == num2)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
