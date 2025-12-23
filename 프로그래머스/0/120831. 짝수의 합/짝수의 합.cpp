2dad0fe93c67a88e10af4a0d156a59a02d01e9af
tkd6966 <tkd6966@gmail.com>
2025-12-23 09:06:59 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -7,10 +7,10 @@ int solution(int n)
 {
     int answer = 0;
     for(int i = 0; i <= n; i++)
-        if(i % 2 == 0)
+    {  if(i % 2 == 0)
         {
-            answer += i;
+             answer += i;
         }
-    
+    }
     return answer;
 }
