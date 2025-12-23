326ba40bc6bfc6e4b805469db134d0d0646b2b06
tkd6966 <tkd6966@gmail.com>
2025-12-23 09:30:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    
+    while(n > 0)
+    {
+        answer += (n % 10);
+        
+        n /= 10;
+    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << answer << endl;
+
+    return answer;
+}
