615cf9776ce1bccfa184b4a3bf5a5042fc630896
tkd6966 <tkd6966@gmail.com>
2026-01-07 09:56:49 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) 
+{
+    for(int i = 0; i < myString.size(); i++)
+    {
+        if(myString[i] == 'A') myString[i] = 'B';
+        else myString[i] = 'A';
+    }
+    if(myString.find(pat) != string::npos) return 1;
+    else return 0;
+
+
+}
