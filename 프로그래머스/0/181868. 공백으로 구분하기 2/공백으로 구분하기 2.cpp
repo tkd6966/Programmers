ad84b74d2eb424fa05983ae5760fafec12480319
tkd6966 <tkd6966@gmail.com>
2026-01-22 19:54:41 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.09 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    string temp_string = "";
+    
+  
+    for (int i = 0; i < my_string.length(); i++)
+    {
+        if (my_string[i] != ' ')
+        {
+            temp_string += my_string[i];
+        }
+        else 
+        {
+            if (temp_string != "")
+            {
+                answer.push_back(temp_string);
+                temp_string = "";
+            }
+        }
+    }
+    if (temp_string != "")
+    {
+        answer.push_back(temp_string);
+    }
+    
+    return answer;
+}
